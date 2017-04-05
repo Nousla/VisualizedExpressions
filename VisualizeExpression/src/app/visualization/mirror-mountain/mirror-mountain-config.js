9de186be1fc5fccb00fb61643a7069478a2c6aeb
Nousla <sirtheorist@gmail.com>
2017-04-05 14:26:35 +0200

    Removed auto-generated files.

--- VisualizeExpression/src/app/visualization/mirror-mountain/mirror-mountain-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var MirrorMountainConfig = (function () {
-    function MirrorMountainConfig() {
-    }
-    return MirrorMountainConfig;
-}());
-exports.MirrorMountainConfig = MirrorMountainConfig;
-//# sourceMappingURL=mirror-mountain-config.js.map
