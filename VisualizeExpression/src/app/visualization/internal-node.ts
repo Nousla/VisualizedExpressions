c9f46de981d8a7d67be274784513fb9685d9deea
Nousla <sirtheorist@gmail.com>
2017-05-17 13:17:36 +0200

    More tests for Mirror Mountain service

--- VisualizeExpression/src/app/visualization/internal-node.ts
+++ VisualizeExpression/src/app/visualization/internal-node.ts
@@ -1,9 +1,49 @@
 export class InternalNode {
-    public text: string;
-    public type: Type;
-    public group: Group;
-    public parent: InternalNode;
-    public children: InternalNode[];
+    private _text: string;
+    private _type: Type;
+    private _group: Group;
+    private _parent: InternalNode;
+    private _children: InternalNode[];
+
+    public get text(): string {
+        return this._text;
+    }
+
+    public set text(text: string) {
+        this._text = text;
+    }
+
+    public get type(): Type {
+        return this._type;
+    }
+
+    public set type(type: Type) {
+        this._type = type;;
+    }
+
+    public get group(): Group {
+        return this._group;
+    }
+
+    public set group(group: Group) {
+        this._group = group;
+    }
+
+    public get parent(): InternalNode {
+        return this._parent;
+    }
+
+    public set parent(parent: InternalNode) {
+        this._parent = parent;
+    }
+
+    public get children(): InternalNode[] {
+        return this._children;
+    }
+
+    public set children(children: InternalNode[]) {
+        this._children = children;
+    }
 }
 
 export const enum Type {
--- VisualizeExpression/src/app/visualization/mirror-mountain/mirror-mountain.service.spec.ts
+++ VisualizeExpression/src/app/visualization/mirror-mountain/mirror-mountain.service.spec.ts
@@ -2,7 +2,7 @@ import { MirrorMountainService } from "./mirror-mountain.service";
 import { UndefinedArgumentException } from "../../exceptions/undefined-argument-exception";
 import { ElementRef } from "@angular/core/";
 import { InternalData } from "../internal-data";
-import { InternalNode } from "../internal-node";
+import { InternalNode, Type as InternalNodeType, Group as InternalNodeGroup } from "../internal-node";
 import VisualizationEventHandler from '../visualization-event-handler';
 
 describe('Mirror Mountain Service suite', () => {
@@ -13,23 +13,22 @@ describe('Mirror Mountain Service suite', () => {
     });
 
     it('#visualize should fail at undefined element reference', () => {
-        let mockInternalData: InternalData = new InternalData(new InternalNode());
+        let internalData: InternalData = new InternalData(new InternalNode());
         let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
-        let exception: UndefinedArgumentException = new UndefinedArgumentException("elementRef");
 
         let visualizeFunction = () => {
-            service.visualize(undefined, mockInternalData, mockEventHandler);
+            service.visualize(undefined, internalData, mockEventHandler);
         }
 
         expect(visualizeFunction).toThrowError(UndefinedArgumentException);
     });
 
     it('#visualize should fail at undefined internal data reference', () => {
-        let mockElementRef: ElementRef = new ElementRef({});
+        let elementRef: ElementRef = new ElementRef({});
         let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
 
         let visualizeFunction = () => {
-            service.visualize(mockElementRef, undefined, mockEventHandler);
+            service.visualize(elementRef, undefined, mockEventHandler);
         }
 
         expect(visualizeFunction).toThrowError(UndefinedArgumentException);
@@ -37,21 +36,109 @@ describe('Mirror Mountain Service suite', () => {
 
     it('#visualize should clear existing content', () => {
         let nativeElement = document.createElement("div");
-        let nativeElementChild = document.createElement("svg");
+        let nativeElementChild = document.createElement("p");
         nativeElement.appendChild(nativeElementChild);
 
-        let mockElementRef: ElementRef = new ElementRef(nativeElement);
-        let mockInternalData: InternalData = new InternalData(new InternalNode());
+        let elementRef: ElementRef = new ElementRef(nativeElement);
+        let internalData: InternalData = new InternalData(new InternalNode());
         let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
 
-        service.visualize(mockElementRef, mockInternalData, mockEventHandler);
+        service.visualize(elementRef, internalData, mockEventHandler);
 
-        expect(nativeElement.childElementCount).toEqual(0);
+        let containsChild = nativeElement.contains(nativeElementChild);
+        expect(containsChild).toEqual(false);
     });
 
+    it('#visualize should add svg element', () => {
+        let nativeElement = document.createElement("div");
 
+        let elementRef: ElementRef = new ElementRef(nativeElement);
+        let internalData: InternalData = new InternalData(new InternalNode());
+        let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
+
+        service.visualize(elementRef, internalData, mockEventHandler);
+
+        let svgElement = nativeElement.querySelector("svg");
+        expect(svgElement).toBeDefined();
+    });
+
+    it('#visualize should add node element', () => {
+        let nativeElement = document.createElement("div");
+
+        let elementRef: ElementRef = new ElementRef(nativeElement);
+
+        let internalNode: InternalNode = new InternalNode();
+        internalNode.text = "x";
+        internalNode.children = [];
+        internalNode.type = InternalNodeType.Variable;
+        internalNode.group = InternalNodeGroup.Symbol;
+
+        let internalData: InternalData = new InternalData(internalNode);
+        let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
+
+        service.visualize(elementRef, internalData, mockEventHandler);
+
+        let svgElement = nativeElement.querySelector("svg");
+        let nodeElement = svgElement.querySelector("g");
+        expect(nodeElement).toBeDefined();
+    });
+
+    it('#visualize should add node element with text', () => {
+        let nativeElement = document.createElement("div");
+
+        let elementRef: ElementRef = new ElementRef(nativeElement);
+
+        let internalNode: InternalNode = new InternalNode();
+        internalNode.text = "x";
+        internalNode.children = [];
+        internalNode.type = InternalNodeType.Variable;
+        internalNode.group = InternalNodeGroup.Symbol;
+
+        let internalData: InternalData = new InternalData(internalNode);
+        let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
+
+        service.visualize(elementRef, internalData, mockEventHandler);
+
+        let svgElement = nativeElement.querySelector("svg");
+        let nodeElement = svgElement.querySelector("g");
+        let textElement = svgElement.querySelector("text");
+        expect(textElement.textContent).toBe("x");
+    });
+
+    it('#visualize should add three node elements', () => {
+        let nativeElement = document.createElement("div");
+
+        let elementRef: ElementRef = new ElementRef(nativeElement);
+
+        let rootNode: InternalNode = new InternalNode();
+        rootNode.text = "+";
+        rootNode.type = InternalNodeType.Equality;
+        rootNode.group = InternalNodeGroup.Operator;
+
+        let childNode1: InternalNode = new InternalNode();
+        childNode1.text = "5";
+        childNode1.type = InternalNodeType.Integer;
+        childNode1.group = InternalNodeGroup.Number;
+
+        let childNode2: InternalNode = new InternalNode();
+        childNode1.text = "2";
+        childNode1.type = InternalNodeType.Integer;
+        childNode1.group = InternalNodeGroup.Number;
+
+        rootNode.children = [childNode1, childNode2];
+
+        let internalData: InternalData = new InternalData(rootNode);
+        let mockEventHandler: MockEmptyVisualizationHandler = new MockEmptyVisualizationHandler();
+
+        service.visualize(elementRef, internalData, mockEventHandler);
+
+        let svgElement = nativeElement.querySelector("svg");
+        let rootElement = svgElement.querySelector("g");
+        let nodeElements = rootElement.querySelectorAll("g");
+        expect(nodeElements.length).toBe(3);
+    });
 });
 
 class MockEmptyVisualizationHandler implements VisualizationEventHandler {
-    selectNode(data: InternalNode): void {}
+    selectNode(data: InternalNode): void { }
 }
